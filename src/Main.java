75eefb5c5abdd8d636000c832afa9c12db5f1f4a
seonghyunban <seonghyun.ban@pm.me>
2024-06-12 23:00:22 -0400

    second practice

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
-import java.lang.reflect.Array;
-
 public class Main {
     public static void main(String[] args) {
-        FizzBuzz.doFizzBuzz(5);
-        String[] arg = {"aaa"};
-        FizzBuzz.main(arg);
+
+        int i = 1;
+
+        while (i < 100) {
+            FizzBuzz.doFizzBuzz(i);
+            i += 1;
+        }
+
+        FizzBuzz.main(new String[0]);
+
 
     }
 }
